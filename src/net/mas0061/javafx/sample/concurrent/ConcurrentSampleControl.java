a3009c8f918ee25ecf6603073d08fc2e020fbd76
mas0061 <mas0061@gmail.com>
2013-05-10 15:51:52 +0900

    不要なViewを削除

--- src/net/mas0061/javafx/sample/concurrent/ConcurrentSampleControl.java
+++ src/net/mas0061/javafx/sample/concurrent/ConcurrentSampleControl.java
@@ -11,23 +11,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import net.mas0061.javafx.sample.child.ChildScreen;
 
 public class ConcurrentSampleControl implements Initializable {
 
   @FXML
   private Label msgLabel;
   @FXML
-  private Button pushButton;
-  @FXML
   private Button startButton;
   @FXML
   private Button stopButton;
-  @FXML
-  private Button childButton;
   @FXML
   private ProgressBar sampleBar;
   @FXML
@@ -37,13 +30,8 @@ public class ConcurrentSampleControl implements Initializable {
 
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
-//    msgLabel.setTextFill(Color.RED);
-//    msgLabel.setText("initialize.");
-//    System.out.println("start -> " + startButton.focusedProperty().toString());
-//    System.out.println("stop  -> " + stopButton.focusedProperty().toString());
-//    System.out.println("push  -> " + pushButton.focusedProperty().toString());
   }
-
+  
   @FXML
   protected void start(ActionEvent event) {
     msgLabel.setTextFill(Color.BLACK);
@@ -86,24 +74,6 @@ public class ConcurrentSampleControl implements Initializable {
       msgLabel.setText("stop");
       task.cancel();
     }
-  }
-
-  @FXML
-  protected void push(ActionEvent event) {
-    msgLabel.setTextFill(Color.BLACK);
-    msgLabel.setText("push");
-  }
-
-  @FXML
-  protected void child(ActionEvent event) {
-	  ChildScreen childScreen = new ChildScreen();
-	  Stage stage = new Stage();
-	  childScreen.start(stage);
-  }
-
-  @FXML
-  protected void releaseKey(KeyEvent event) {
-	msgLabel.setText(event.getCode().toString());
   }
 
 }
